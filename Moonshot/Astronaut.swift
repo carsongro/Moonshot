2d2bfd1987e7c3b07d7a064a7175c75176caa7e8
Carson <carsongro@gmail.com>
2022-06-08 14:09:08 -0600

    made main screen

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Carson Gross on 6/8/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
